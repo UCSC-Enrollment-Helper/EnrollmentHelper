041aa93fe1ee2c2fd4353915ac9cd52865c4b368
mukhulm4 <mukhulm4@gmail.com>
2024-01-21 02:04:18 -0800

    merging branches

--- professor_stats.py
+++ professor_stats.py
@@ -15,27 +15,37 @@ if response.status_code == 200:
     #Specify the CSS selector of the specific div you want to scrape
     instructor_divs = soup.find_all("div", {"class": "instructor"})
 
-    #print(instructor_divs)
-    index = 1
-    for instructor_div in instructor_divs:
+        index = 1
+        prof_stats = ""
 
-        instructor_info = instructor_div.text.strip()
-        profName = instructor_info.replace("Instructor", "").strip()
-        #print(profName)
+        for instructor_div in instructor_divs:
+            instructor_info = instructor_div.text.strip()
+            prof_name = instructor_info.replace("Instructor", "").strip()
 
-    #Find the div using BeautifulSoup
-        professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("UCSC"), profName)
+            # Find the div using BeautifulSoup
+            professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("UCSC"), prof_name)
 
-        
-        if professor is not None:
-            print("Instructor: ", index)
-            print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-            print("Rating: %s / 5.0" % professor.rating)
-            print("Difficulty: %s / 5.0" % professor.difficulty)
-            print("Total Ratings: %s" % professor.num_ratings)
-            if professor.would_take_again is not None:
-                print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
-            else:
-                print("Would Take Again: N/A")
-            index+=1
-            print("")
+            if professor is not None:
+                prof_stats += f"Instructor: {index}\n"
+                prof_stats += f"{professor.name} works in the {professor.department} Department of {professor.school.name}.\n"
+                prof_stats += f"Rating: {professor.rating} / 5.0\n"
+                prof_stats += f"Difficulty: {professor.difficulty} / 5.0\n"
+                prof_stats += f"Total Ratings: {professor.num_ratings}\n"
+                if professor.would_take_again is not None:
+                    prof_stats += f"Would Take Again: {round(professor.would_take_again, 1)}%\n"
+                else:
+                    prof_stats += "Would Take Again: N/A\n"
+                prof_stats += "\n"
+                index += 1
+            if(index == 10):
+                break
+
+        return prof_stats
+    else:
+        print(f"Failed to retrieve the webpage. Status Code: {response.status_code}")
+        return None
+
+prof_stats_string = run(url)
+
+# Print the string
+print(prof_stats_string)
